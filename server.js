12e1bab81095f0721be61c57eb87343baa7c4aba
sai suraj <saisuraj1537@gmail.com>
2025-04-06 22:58:10 +0530

    Switch to express server for Render deploy

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 
 // ✅ Initialize Firebase Admin SDK
@@ -131,6 +138,6 @@ app.get("/protected", verifyJWT, (req, res) => {
   res.json({ message: "Access Granted", user: req.user });
 });
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
